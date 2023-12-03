267c792d49c2f96c999b3981cee8f64d16fe5d65
W@r_d0g <137320379+d0nt-sm1le@users.noreply.github.com>
2023-12-03 12:04:49 -0300

    .

--- exercicios/output/advinha.c
+++ exercicios/output/advinha.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <locale.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    printf ("OlÃ¡ mundo");
-    return 0;
+    setlocale(LC_ALL, "Portuguese");
+    system("cls");
+
+    int chute;
+
+    printf("*************************\n");
+    printf("* Bem vindo ao meu jogo *\n");
+    printf("*************************\n"); 
+    
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Você chutou o número %d", chute);
+
 }
